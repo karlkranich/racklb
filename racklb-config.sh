30fb24550b69fa6c8d0c901b46b23b672f613cb9
Karl Kranich <karl.kranich@gmail.com>
2016-02-24 14:52:06 -0500

    add comments to config script

--- racklb-config.sh
+++ racklb-config.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Config variables for racklb.sh
 
+# Account number from "Account Details" section of Account Settings page
 rackAccount=
+
+# Username from "Login Details" section of Account Settings page
 rackUser=
+
+# API Key from "Login Details" section of Account Settings page
 rackKey=
